616f96640112f80bc290d38f59d77d9e3429593c
Todor <1@todor.one>
2017-07-21 21:47:49 +0300

    Added user descriptions from scraped data of StarWars API

--- server/src/controllers/USERS.js
+++ server/src/controllers/USERS.js
@@ -1,13 +1,14 @@
 const _ = require('lodash')
 const { getRandomInteger } = require('./utils')
 const userIds = require('./data/userIds')
+const userDescriptions = require('./data/userDescriptions')
 
 const USERS = {}
 
 for (let i = 0; i < 50; i++) {
   USERS[userIds[i]] = {
     id: userIds[i],
-    description: 'Hi there!',
+    description: userDescriptions[i],
     thumbUrl: `https://0.s3.envato.com/files/97977535/128/${getRandomInteger(1, 15)}_resize.png`,
     following: [],
   }
